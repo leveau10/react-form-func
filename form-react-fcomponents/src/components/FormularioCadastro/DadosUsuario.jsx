94b10c161a402df7bf8a474c5944c49fdf80e309
Leonardo Viana <leovir957@gmail.com>
2021-07-28 20:49:07 -0300

    useEffect

--- form-react-fcomponents/src/components/FormularioCadastro/DadosUsuario.jsx
+++ form-react-fcomponents/src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TextField, Button } from "@material-ui/core";
 
 function DadosUsuario({ aoEnviar }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        aoEnviar();
+        aoEnviar({ email, senha });
       }}
     >
       <TextField
+        value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
         id="email"
         label="E-mail"
         type="email"
@@ -19,6 +26,10 @@ function DadosUsuario({ aoEnviar }) {
         required
       />
       <TextField
+        value={senha}
+        onChange={(event) => {
+          setSenha(event.target.value);
+        }}
         id="senha"
         label="Senha"
         type="password"
